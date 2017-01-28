74ae6236401c1a9dbcd1097ae29df4b9c159a6db
ipanardian <ipan.software@gmail.com>
2017-01-28 21:38:03 +0700

    Author credit

--- app.php
+++ app.php
@@ -1,8 +1,18 @@
 <?php
+/**
+*  TukangAbsen
+*  (c) 2017 Ipan Ardian
+*
+*  When engineers doesn't want to fill out attendance manually, tukangAbsen is the answer!
+*  For details, see the web site: https://github.com/ipanardian/tukangAbsen
+*  The GPL 3.0 License
+*/
+
 if (isset($_SERVER['REMOTE_ADDR'])) die('Permission denied.');
 error_reporting(E_ALL ^E_NOTICE);
 
-class TukangAbsen {
+class TukangAbsen 
+{
     protected $arg;
     protected $command;
     protected $CODECEPTION_ARG;
--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
+/**
+*  TukangAbsen
+*  (c) 2017 Ipan Ardian
+*
+*  When engineers doesn't want to fill out attendance manually, tukangAbsen is the answer!
+*  For details, see the web site: https://github.com/ipanardian/tukangAbsen
+*  The GPL 3.0 License
+*/
+
 require('./app.php');
 
 (new TukangAbsen($argv))->run();
